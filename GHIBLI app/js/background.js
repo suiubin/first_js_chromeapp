d0cea187401154cdba4b5e9d9511a09d40586a4a
suiubin <86407726+suiubin@users.noreply.github.com>
2021-08-27 00:40:12 +0900

    Create background.js

---
+++ GHIBLI app/js/background.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 10;
+
+function paintImg(imgNumber){
+    const image = new Image();
+    image.src = `img_js/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+function getRandom() {
+    const number = Math.floor(Math.random()*IMG_NUMBER);
+    return number;
+}
+function init(){
+    const randomNumber = getRandom();
+    paintImg(randomNumber);
+}
+init();
